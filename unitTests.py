22cd304d9854253ab1d9d7ea95680b4d5f05d44a
Minh Le <131617436+minhdarine@users.noreply.github.com>
2024-05-12 21:09:21 +0200

    Update unitTests.py

--- unitTests.py
+++ unitTests.py
@@ -33,8 +33,9 @@ class TestMain(unittest.TestCase):
             ['', 'r', 'r', 'r', 'r', 'r', 'r', ''],
             [None, 'r', 'r', 'r', 'r', 'r', 'r', None]
             ]     
-        expected = [34,'B7-A7','B7-C7','B7-B6','C7-B7','C7-D7','C7-C6','D7-C7','D7-E7','D7-D6','E7-D7','E7-F7','E7-E6','F7-E7','F7-G7','F7-F6','G7-F7','G7-H7','G7-G6','B8-C8','B8-B7','C8-B8','C8-D8','C8-C7','D8-C8','D8-E8','D8-D7','E8-D8','E8-F8','E8-E7','F8-E8','F8-G8','F8-F7','G8-F8','G8-G7']
-        self.assertEqual(get_move_list(gameboard, 'b'), expected)
+        tested = get_move_list(gameboard, 'b')
+        expected = {34,'B7-A7','B7-C7','B7-B6','C7-B7','C7-D7','C7-C6','D7-C7','D7-E7','D7-D6','E7-D7','E7-F7','E7-E6','F7-E7','F7-G7','F7-F6','G7-F7','G7-H7','G7-G6','B8-C8','B8-B7','C8-B8','C8-D8','C8-C7','D8-C8','D8-E8','D8-D7','E8-D8','E8-F8','E8-E7','F8-E8','F8-G8','F8-F7','G8-F8','G8-G7'}
+        self.assertEqual(set(tested), expected)
 
     #def test_get_move(self):
     #    move = switch_char(x) + str(y+1) +'-'+ switch_char(x_new) + str(y_new+1)
@@ -51,6 +52,7 @@ class TestMain(unittest.TestCase):
             [None, '', '', '', 'r', '', '', None]
             ]    
         expected = True
+        
 obj = TestMain()
 print(obj.test_fen_to_available_moves())
 print(obj.test_generate_gameboard())
